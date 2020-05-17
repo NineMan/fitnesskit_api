fc1105f88450eb5e3af147c7bce5986135cf182d
NineMan <nelubinmv@inbox.ru>
2020-05-17 17:01:51 +0400

    Create models Teacher and Course

--- course/models.py
+++ course/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+
+    name = models.CharField(max_length=255)
+    short_name = models.CharField(max_length=50)
+    position = models.CharField(max_length=100)
+    imageUrl = models.URLField()
+
+    def __str__(self):
+        return self.short_name
+
+
+class Course(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    place = models.CharField(max_length=50)
+    teacher = models.CharField(max_length=255)
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    weekDay = models.PositiveSmallIntegerField()
+    appointment_id = models.CharField(max_length=50)
+    service_id = models.CharField(max_length=50)
+    pay = models.BooleanField()
+    appointment = models.BooleanField()
+    teacher_v2 = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING, related_name='teachers_v2')
+    color = models.CharField(max_length=15)
+    availability = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.name
